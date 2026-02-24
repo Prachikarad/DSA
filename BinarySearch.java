1899ae026d9568762ec3d2973c871e0ee9426c59
Prachi Karad <karadprachi10@gmail.com>
2026-02-24 11:51:18 +0530

    Added Binary Search implementation in Java

---
+++ BinarySearch.java
@@ -0,0 +1,42 @@
+public class BinarySearch {
+
+    // Function to perform binary search
+    public static int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            // If target is found
+            if (arr[mid] == target) {
+                return mid;
+            }
+            // If target is greater, ignore left half
+            else if (arr[mid] < target) {
+                left = mid + 1;
+            }
+            // If target is smaller, ignore right half
+            else {
+                right = mid - 1;
+            }
+        }
+
+        // Target not found
+        return -1;
+    }
+
+    // Main method
+    public static void main(String[] args) {
+        int[] arr = {2, 5, 8, 12, 16, 23, 38, 56, 72};
+        int target = 23;
+
+        int result = binarySearch(arr, target);
+
+        if (result != -1) {
+            System.out.println("Element found at index: " + result);
+        } else {
+            System.out.println("Element not found");
+        }
+    }
+}
